9ed5af0ac3ca0d3a1b2c37786240aa3dbf2ac775
muthukumarchinnapudaiyar <muthukumarchinnapudaiyar@gmail.com>
2026-02-23 11:21:26 +0530

    Initial Commit

---
+++ windowsHandles.java
@@ -0,0 +1,29 @@
+package SeleniumPractice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class windowsHandles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://leafground.com/window.xhtml");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//span[text()='Open Multiple']")).click();
+		Set<String> AllpageID=driver.getWindowHandles();
+		System.out.println(AllpageID);
+		List<String> PageList=new ArrayList<String>(AllpageID);
+		driver.switchTo().window(PageList.get(2));
+		driver.findElement(By.xpath("//input[@placeholder='E-mail Address']")).sendKeys("mk8051550@gmail.com");
+		
+
+	}
+
+}
